1f91caf5ffabc4ee4ae8159d81177c8afad63529
kvadrick <nmsmet@ya.ru>
2022-07-08 21:15:54 +0300

    Add folder 002_Hw

---
+++ 002_Hw/Program.cs
@@ -0,0 +1,22 @@
+﻿int n;
+Console.WriteLine("Напишите количество чисел: ");
+n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-10, 10);
+}
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+
+int sum = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (i % 2 == 1)
+    {
+        sum = sum + array[i];
+        i++;
+    }
+}
+Console.WriteLine(sum);
+
