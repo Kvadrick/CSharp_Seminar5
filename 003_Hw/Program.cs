5394366c20ce5d576aafa29de8b7fc970b219d57
kvadrick <nmsmet@ya.ru>
2022-07-08 21:16:06 +0300

    Add folder 003_Hw

---
+++ 003_Hw/Program.cs
@@ -0,0 +1,32 @@
+﻿int n;
+Console.WriteLine("Введите количество чисел: ");
+n = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[n];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = Convert.ToDouble(new Random().Next(10, 1000) / 100.0);
+   
+}
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+
+
+double max = array[0], min = array[0], c = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i] > max) {
+        
+    max = array[i];
+    }
+    if(min > array[i]){
+
+        min = array[i];
+    }
+    c = max - min;
+}
+
+Console.WriteLine($"max = {max}");
+Console.WriteLine($"min = {min}");
+Console.WriteLine($"Разность = {c}");
+
